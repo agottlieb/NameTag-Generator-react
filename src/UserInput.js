9f4fdc0f3fb14665d89f5f99e7fd2f0d7ed470d6
agottlieb <allison.m.gottlieb@gmail.com>
2020-08-21 16:21:16 -0400

    added handler and listener for typing in name

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
 
 class UserInput extends Component {
+  state = {
+    name: ""
+  };
+
+  //event handler for typing in name field
+  updateName = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
   render() {
     return (
       <form>
-        <input type="text" placeholder="Add a new name here..." />
+        <input
+          type="text"
+          placeholder="Add a new name here..."
+          value={this.state.name}
+          /*event listener for name typing box*/
+          onChange={this.updateName}
+        />
         <input type="submit" value="Create Name Tag" />.
       </form>
     );
