046cf2dc44668d9aebd11b602033ce2dd1d7d04d
agottlieb <allison.m.gottlieb@gmail.com>
2020-08-21 16:05:04 -0400

    created user input module

---
+++ src/UserInput.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class UserInput extends Component {
+  <form>
+  <input type='text' placeholder='Add a new name here...' /> 
+  <input type='submit' value='Create Name Tag' />.
+  </form>
+}
+
+export default UserInput;
